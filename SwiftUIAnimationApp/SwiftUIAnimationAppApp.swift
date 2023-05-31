d77bdbab94b6baea54c6ad501f322949e1a16dbe
A.Vassel <102523970+AVassel@users.noreply.github.com>
2023-05-31 13:47:02 +0300

    Initial Commit

---
+++ SwiftUIAnimationApp/SwiftUIAnimationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationAppApp.swift
+//  SwiftUIAnimationApp
+//
+//  Created by Anton Vassel on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
